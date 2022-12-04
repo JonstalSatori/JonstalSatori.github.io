f606a155d63292447ee34babd01fc01850654a35
Jonstal Satori <jonstalthebirdy@gmail.com>
2022-12-03 20:26:42 -0500

    commit message

---
+++ JavaScripts/Numbers.js
@@ -0,0 +1,8 @@
+//Variables : Can be using var "NAME"
+
+let Name = "Satori"; //  string literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined; // undefiend var
+let lastName = null; // Clear vaule of var (Later changed)
+
